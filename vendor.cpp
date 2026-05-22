3d3aa610a74c4543bc4e6d7a63c00c33823fefac
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-05-21 19:52:31 -0700

    Milestone 1

---
+++ vendor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+struct Node{
+    string name;
+    string order;
+    Node *next;
+};
+
+int main(){
+
+    //generated data arrays
+    string customerNames[] = {
+        "Ava", "Noah", "Mia", "Liam", "Emma",
+        "Sophia", "Lucas", "Olivia", "Ethan", "Isabella",
+        "Mason", "Charlotte", "Elijah", "Amelia", "James"
+    };
+
+    string drinkOrders[] = {
+        "Latte", "Cappuccino", "Espresso", "Americano", "Mocha",
+        "Caramel Macchiato", "Cold Brew", "Iced Coffee", "Chai Latte", "Flat White"
+    };
+    return 0;
+}
